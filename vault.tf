cc07e0c3a6aa23818961b3a72fd7cfe728a17553
Simon Lynch <simon.lynch@hashicorp.com>
2023-02-16 14:08:18 +1100

    initial vault

---
+++ vault.tf
@@ -0,0 +1,20 @@
+
+
+locals {
+    hcp_vault_public_fqdn = module.hcp-vault.public_endpoint_url
+    vault_token = module.hcp-vault.admin_token
+
+}
+
+resource "vault_mount" "pki-example" {
+  depends_on = [
+    local.hcp_vault_public_fqdn
+  ]
+
+  path        = "pki"
+  type        = "pki"
+  description = "This is an example PKI mount"
+
+  default_lease_ttl_seconds = 3600
+  max_lease_ttl_seconds     = 86400
+}
