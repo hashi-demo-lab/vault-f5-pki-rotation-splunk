a4ac74e9ed51888f0745dcc0682873497874ead6
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-23 13:52:33 +1100

    dns

--- modules/aws/vpc.tf
+++ modules/aws/vpc.tf
@@ -12,6 +12,7 @@ module "vpc" {
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     "kubernetes.io/cluster/${var.deployment_id}" = "shared"
