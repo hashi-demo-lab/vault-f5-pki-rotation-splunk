33c9716fc8be27b501d100e0ed741cf967c3e6bb
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-28 11:46:13 +1100

    update conditional

--- F5virtual/virtualserverSSL/main.tf
+++ F5virtual/virtualserverSSL/main.tf
@@ -104,7 +104,6 @@ output "log_cert" {
 ### Validation Example
 
 data "tls_certificate" "this" {
-  count = vault_pki_secret_backend_cert.this.renew_pending ? 1 : 0
   url = "https://${var.common_name}"
 
   lifecycle {
