1e87266c8225e510e4b38a3bbf87021d05e2dd46
Simon Lynch <simon.lynch@hashicorp.com>
2023-03-28 11:29:11 +1100

    update monitor none

--- F5virtual/virtualserverSSL/main.tf
+++ F5virtual/virtualserverSSL/main.tf
@@ -77,6 +77,7 @@ resource "bigip_ltm_node" "node" {
   for_each = toset(var.node_list)
   name     = "/Common/${each.value}"
   address  = each.value
+  monitor = "none"
 }
 
 resource "bigip_ltm_pool_attachment" "attach_node" {
