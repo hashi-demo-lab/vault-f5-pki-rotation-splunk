eea7c39b24598766acb262e5c7b1fcf163fa8b41
Patrick <pcarey@hashicorp.com>
2023-04-28 09:29:54 +1000

    fixing

--- modules/splunk/hashidemo.tf
+++ modules/splunk/hashidemo.tf
@@ -1,3 +1,8 @@
+data "aws_route53_zone" "sbx_hashidemos_io" {
+  name = "${var.prefix}.sbx.hashidemos.io"
+}
+
+
 resource "aws_route53_record" "splunk" {
   zone_id = data.aws_route53_zone.sbx_hashidemos_io.zone_id
   name    = "${var.splunk_domain}.${var.prefix}.sbx.hashidemos.io"
